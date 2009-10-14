33d4bf821b7aecfc5a7e834d92b6fbaadac7550d
Siegfried Schmitz <ss@symmetrics.de>
2009-10-14 09:41:34 +0200

    code cleanup

--- build/app/code/local/Symmetrics/TrustedRating/Block/Widget.php
+++ build/app/code/local/Symmetrics/TrustedRating/Block/Widget.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * Symmetrics_TrustedRating_Block_Widget
+ *
+ * @category Symmetrics
+ * @package Symmetrics_TrustedRating
+ * @author symmetrics gmbh <info@symmetrics.de>, Siegfried Schmitz <ss@symmetrics.de>
+ * @copyright symmetrics gmbh
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class Symmetrics_TrustedRating_Block_Widget extends Mage_Core_Block_Template
 {
-
     protected function _construct()
     {
         parent::_construct();
@@ -9,12 +17,12 @@ class Symmetrics_TrustedRating_Block_Widget extends Mage_Core_Block_Template
 
     protected function _toHtml()
     {
-    	$model = new Symmetrics_TrustedRating_Model_TrustedRating();
-		if($model->getIsActive()) {
+    	$model = Mage::getModel('trustedrating/trustedrating');
+		if ($model->getIsActive()) {
 			return $model->getImage();
 		}
     	else {
-    		return '';
+    		return null;
     	}
     }
 }
