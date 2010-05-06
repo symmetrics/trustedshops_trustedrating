d2540ddb6a40e61bc574edeb3847ed35aa600ad0
Siegfried Schmitz <ss@symmetrics.de>
2010-05-06 15:44:28 +0200

    Fehler behoben, der Registrierungslinks wurder per js nicht im Kommentarfeld in der Konfiguration angezeigt. Kompatibilität mit 1.4.0.0 und 1.4.0.1 in die info.py eingetragen.

--- info.py
+++ info.py
@@ -59,6 +59,6 @@ DEPENDS_ON_FILES = ()
 PEAR_KEY = ''
 
 COMPATIBLE_WITH = {
-     'magento': ['1.3.2.3'],
+     'magento': ['1.3.2.3', '1.4.0.0', '1.4.0.1'],
      'magento_enterprise': ['1.3.2.4'],
 }
