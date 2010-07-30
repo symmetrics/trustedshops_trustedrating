5cfaf2de4d547a799667496a2764d533a04c6092
Eugen Gitin <eg@symmetrics.de>
2010-07-30 16:10:02 +0200

    Removed compatibility to 1.3; Moved ACLs from config.xml to adminhtml.xml;

--- info.py
+++ info.py
@@ -59,6 +59,6 @@ DEPENDS_ON_FILES = ()
 PEAR_KEY = 'magento-community/symmetrics_trustedrating'
 
 COMPATIBLE_WITH = {
-     'magento': ['1.3.2.3', '1.4.0.0', '1.4.0.1'],
-     'magento_enterprise': ['1.3.2.4'],
+     'magento': ['1.4.0.0', '1.4.0.1'],
+     'magento_enterprise': ['1.7.0.0'],
 }
