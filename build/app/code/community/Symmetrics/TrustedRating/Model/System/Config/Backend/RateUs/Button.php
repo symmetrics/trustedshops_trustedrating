e3000a476211521a2b62857ac726e28cf6b54b12
Ngoc Anh Doan <nd@symmetrics.de>
2013-12-23 22:00:21 +0100

    SUPTRUSTEDSHOPS-129 - Removed superfluous character.

--- build/app/code/community/Symmetrics/TrustedRating/Model/System/Config/Backend/RateUs/Button.php
+++ build/app/code/community/Symmetrics/TrustedRating/Model/System/Config/Backend/RateUs/Button.php
@@ -279,7 +279,7 @@ class Symmetrics_TrustedRating_Model_System_Config_Backend_RateUs_Button extends
             }
 
             foreach ($_stores as $store) {
-                /** @var $store Mage_Core_Model_Store */#
+                /** @var $store Mage_Core_Model_Store */
                 if ($this->getHelper()->isTrustedRatingActive($store)) {
                     $stores[] = $store;
                 }
