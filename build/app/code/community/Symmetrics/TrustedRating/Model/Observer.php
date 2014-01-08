af93d15e295801853a46b6ac945cc518cbde238f
Ngoc Anh Doan <nd@symmetrics.de>
2014-01-08 13:15:58 +0100

    SUPTRUSTEDSHOPS-145 - Mannually merged missing changes of SUPTRUSTEDSHOPS-145-deprecated-installation-routines into dev.

--- build/app/code/community/Symmetrics/TrustedRating/Model/Observer.php
+++ build/app/code/community/Symmetrics/TrustedRating/Model/Observer.php
@@ -19,13 +19,19 @@
  * @author    Yauhen Yakimovich <yy@symmetrics.de>
  * @author    Toni Stache <ts@symmetrics.de>
  * @author    Ngoc Anh Doan <ngoc-anh.doan@cgi.com>
- * @copyright 2009-2013  symmetrics - a CGI Group brand
+ * @copyright 2009-2014 symmetrics - a CGI Group brand
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://github.com/symmetrics/trustedshops_trustedrating/
  * @link      http://www.symmetrics.de/
+ * @link      http://www.de.cgi.com/
  */
 
 /**
  * Observer model
+ *
+ * SUPTRUSTEDSHOPS-129: Submission of reminder emails from the Magento host is replaced by
+ * Trusted Shops' 'Rate later' service thus the event listener checkSendRatingEmail and the
+ * used methods are superfluous.
  *
  * @category  Symmetrics
  * @package   Symmetrics_TrustedRating
@@ -34,9 +40,11 @@
  * @author    Yauhen Yakimovich <yy@symmetrics.de>
  * @author    Toni Stache <ts@symmetrics.de>
  * @author    Ngoc Anh Doan <ngoc-anh.doan@cgi.com>
- * @copyright 2009-2013  symmetrics - a CGI Group brand
+ * @copyright 2009-2014 symmetrics - a CGI Group brand
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://github.com/symmetrics/trustedshops_trustedrating/
  * @link      http://www.symmetrics.de/
+ * @link      http://www.de.cgi.com/
  */
 class Symmetrics_TrustedRating_Model_Observer
 {
@@ -44,6 +52,7 @@ class Symmetrics_TrustedRating_Model_Observer
      * Config path to email template id
      *
      * @var string
+     * @deprecated since v0.2.4
      */
     const XML_PATH_CONFIG_EMAIL_TEMPLATE = 'trustedrating/trustedrating_email/template';
 
@@ -69,115 +78,6 @@ class Symmetrics_TrustedRating_Model_Observer
         $returnValue = $this->_callTrustedShopsApi($sendData, $soapUrl);
         $returnString = Mage::helper('trustedrating')->__('TrustedShops return value: ');
         Mage::getSingleton('core/session')->addNotice($returnString . $returnValue);
-    }
-
-    /**
-     * Checks the shippings which will get an email
-     *
-     * @return void
-     */
-    public function checkSendRatingEmail()
-    {
-        $model = Mage::getModel('trustedrating/trustedrating');
-        if ($shipmentIds = $model->checkShippings()) {
-            $this->_sendTrustedRatingMails($shipmentIds);
-        }
-    }
-
-    /**
-     * Send mail and save entry to db
-     *
-     * @param array $shipmentIds Shipment IDs
-     *
-     * @return void
-     */
-    private function _sendTrustedRatingMails($shipmentIds)
-    {
-       foreach ($shipmentIds as $shipmentId) {
-           $orderId = $this->getHelper()->getOrderId($shipmentId);
-           $order = Mage::getModel('sales/order')->load($orderId);
-           $this->_sendTransactionalMail($order);
-           $this->_saveShipmentIdToTable($shipmentId);
-       }
-    }
-
-    /**
-     * Generates the rating url by given order object.
-     *
-     * @param Mage_Sales_Model_Order $order Order object.
-     *
-     * @return string
-     */
-    protected function _getRatingUrl($order)
-    {
-        $trustedRating = Mage::getSingleton('trustedrating/trustedrating');
-        $storeId = $order->getStoreId();
-        $tsId = $trustedRating->getTsId($storeId);
-        $params = array(
-            'buyerEmail' => base64_encode($order->getCustomerEmail()),
-            'shopOrderID' => base64_encode($order->getRealOrderId())
-        );
-        $ratingUrl = $trustedRating->getEmailRatingLink($storeId) . '_' . $tsId . '.html'
-                   . '&'    // Do not change, it's necessary for the TS systems to
-                            // handle the query parameters correctly.
-                   . http_build_query($params);
-
-        return $ratingUrl;
-    }
-
-    /**
-     * Generates the image url by given order object.
-     *
-     * @param Mage_Sales_Model_Order $order Order object.
-     *
-     * @return string
-     */
-    protected function _getImageUrl($order)
-    {
-        $language = Mage::getStoreConfig(
-            Symmetrics_TrustedRating_Model_Trustedrating::CONFIG_LANGUAGE,
-            $order->getStoreId()
-        );
-        
-        return Mage::getDesign()->getSkinUrl(
-            'images/bewerten_' . $language . '.gif', array('_area' => 'frontend')
-        );
-    }
-
-    /**
-     * Send transactional email to customer.
-     *
-     * @param Mage_Sales_Model_Order $order Order object.
-     *
-     * @return void
-     */
-    public function _sendTransactionalMail($order)
-    {
-        Mage::getModel('core/email_template')->sendTransactional(
-            Mage::getStoreConfig(self::XML_PATH_CONFIG_EMAIL_TEMPLATE, $order->getStoreId()),
-            Mage::getStoreConfig(self::XML_PATH_EMAIL_IDENTITY),
-            $order->getCustomerEmail(),
-            $order->getCustomerName(),
-            array(
-                'ratingUrl' => $this->_getRatingUrl($order),
-                'imageUrl' => $this->_getImageUrl($order)
-            ),
-            $order->getStoreId()
-        );
-    }
-
-    /**
-     * Save shipping ID of customers which got an email to table
-     *
-     * @param int $shipmentId Shipping Id
-     *
-     * @return void
-     */
-    private function _saveShipmentIdToTable($shipmentId)
-    {
-        $mailModel = Mage::getModel('trustedrating/mail');
-        $mailModel->setShippmentId($shipmentId)
-            ->save();
     }
 
     /**
